a3f756b37ab6a7e72b40603e4270847344a1be15
Marcelloax <marcellomachado2014@gmail.com>
2025-10-06 16:23:07 -0300

    pokemon atualizado casa

--- pokemon/script.js
+++ pokemon/script.js
@@ -6,16 +6,24 @@ async function getPokemon(){
     const p = document.createElement("p")
     p.innerText = infos.name
     const sprite = document.createElement("img")
-    sprite.src = infos.sprites.front_default
+    sprite.src = infos.sprites.versions["generation-v"]["black-white"].animated.front_default
     
     const repo = await fetch(infos.species.url)
     const info = await repo.json()
     console.log(info,"species")
 
-    const k = document.createElement("p")
-    k.innerText = info.base_happiness
+    const entry = info.flavor_text_entries.find(e =>
+    e.language.name === "pt" || e.language.name === "en");
+    const historia = document.createElement("p");
+    historia.classList.add("historia");
+    historia.innerText = entry ?
+    entry.flavor_text.replace(/\f/g, " ") : "historia não encontrada.";
 
-    
-    div.append(p,sprite,k)
+    const tipo = infos.types[0].type.name;
+  const backgrounds = document.createElement("div");
+  backgrounds.classList.add("pokemon-background", tipo);
+
+  backgrounds.append(p, sprite,historia);
+  div.append(backgrounds);
 }
 getPokemon()
